99df4b9a370553080a44f84d41ea5ca48f399e42
Dolmake <demondolmake@hotmail.com>
2015-05-05 09:33:44 +0200

    GUI Button. OnButtonTapped

---
+++ DolmakeTowerDefense/Assets/Game/Scripts/SGameGUI/OnTapButton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnTapButton : MonoBehaviour {
+
+	void OnEnable()
+    {
+        InputServer.SINGLETON.OnTouchCollider += HandleOnTouchCollider;
+    }
+
+    void OnDisable ()
+	{
+		InputServer.SINGLETON.OnTouchCollider -= HandleOnTouchCollider;
+	}
+
+	void HandleOnTouchCollider (RaycastHit obj)
+	{
+		if (obj.collider.transform == this.transform) { //Is me
+			Debug.Log("Button tapped");
+		}
+	}
+}
